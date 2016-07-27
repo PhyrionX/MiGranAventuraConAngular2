acae43de905a6190db6c7e0b3f7d9813a81c41d5
phyrionx <rgcmb@hotmail.com>
2016-07-27 17:48:45 +0200

    Example finish, work

--- app/main.ts
+++ app/main.ts
@@ -1,6 +1,7 @@
 /**
  * Created by phyrion on 24/07/16.
  */
+import 'rxjs/add/operator/map'
 import { bootstrap } from '@angular/platform-browser-dynamic';
 import AppComponent from './app.component';
 
